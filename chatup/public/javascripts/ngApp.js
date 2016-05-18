86044d871a88ec6aaf93bf0305af7e0c4f96b556
aminkh17 <amin.17@gmail.com>
2016-05-18 23:52:11 +0800

    ngView and ngAnimate

--- chatup/public/javascripts/ngApp.js
+++ chatup/public/javascripts/ngApp.js
@@ -1,7 +1,23 @@
-﻿(function ()
-{
+﻿(function (angular){
     'use strict';
-    var app = angular.module('chatApp', []);
+    var app = angular.module('chatApp', ['ngRoute', 'ngAnimate'])
+    .config(['$routeProvider', '$locationProvider',
+        function ($routeProvider, $locationProvider)
+        {
+            $routeProvider
+        .when('/Book/:bookId', {
+                templateUrl: 'book.html',
+                controller: 'BookCtrl',
+                controllerAs: 'book'
+            })
+        .when('/Book/:bookId/ch/:chapterId', {
+                templateUrl: 'chapter.html',
+                controller: 'ChapterCtrl',
+                controllerAs: 'chapter'
+            });
+            
+            $locationProvider.html5Mode(true);
+        }]);
 
 
-})();
+})(window.angular);
